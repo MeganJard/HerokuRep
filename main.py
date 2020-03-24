84927c9b220d5291e940020a121dce601471f545
MeganJard <57149105+MeganJard@users.noreply.github.com>
2020-03-24 16:08:16 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('Прив')
+from flask import Flask
+from flask_ngrok import run_with_ngrok
+
+app = Flask(__name__)
+run_with_ngrok(app)
+
+
+@app.route("/")
+def index():
+    return "Привет от приложения Flask"
+
+
+if __name__ == '__main__':
+    app.run()
